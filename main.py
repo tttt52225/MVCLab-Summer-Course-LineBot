32ea358f05a021c4224e31388ad3bfd91c669fc3
pikachu <tttt52225@ymail.com>
2022-08-22 16:51:04 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,123 @@
+import os
+import re
+import json
+import random
+from dotenv import load_dotenv
+from pyquery import PyQuery
+from fastapi import FastAPI, Request, HTTPException
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+
+load_dotenv() # Load your local environment variables
+
+CHANNEL_TOKEN = os.environ.get('LINE_TOKEN')
+CHANNEL_SECRET = os.getenv('LINE_SECRET')
+
+app = FastAPI()
+
+My_LineBotAPI = LineBotApi(CHANNEL_TOKEN) # Connect Your API to Line Developer API by Token
+handler = WebhookHandler(CHANNEL_SECRET) # Event handler connect to Line Bot by Secret key
+
+CHANNEL_ID = os.getenv('LINE_UID') # For any message pushing to or pulling from Line Bot using this ID
+
+'''
+See more about Line Emojis, references below
+> Line Bot Free Emojis, https://developers.line.biz/en/docs/messaging-api/emoji-list/
+'''
+# Create my emoji list
+my_emoji = [
+    [{'index':27, 'productId':'5ac1bfd5040ab15980c9b435', 'emojiId':'005'}],
+    [{'index':27, 'productId':'5ac1bfd5040ab15980c9b435', 'emojiId':'019'}],
+    [{'index':27, 'productId':'5ac1bfd5040ab15980c9b435', 'emojiId':'096'}]
+]
+
+# Line Developer Webhook Entry Point
+@app.post('/')
+async def callback(request: Request):
+    body = await request.body() # Get request
+    signature = request.headers.get('X-Line-Signature', '') # Get message signature from Line Server
+    try:
+        handler.handle(body.decode('utf-8'), signature) # Handler handle any message from LineBot and 
+    except InvalidSignatureError:
+        raise HTTPException(404, detail='LineBot Handle Body Error !')
+    return 'OK'
+
+# All message events are handling at here !
+@handler.add(MessageEvent, message=TextMessage)
+def handle_textmessage(event):
+    # global my_pokemons
+    ''' Basic Message Reply
+    message = TextSendMessage(text= event.message.text)
+    My_LineBotAPI.reply_message(
+        event.reply_token,
+        message
+    )
+    '''
+    formula = event.message.text
+    flag = False 
+    numbers = "0123456=-*/+"
+    curStr = ""
+    for c in formula:
+        if c != " ":
+            curStr += c 
+    formula = curStr
+    for c in formula:
+        if c not in numbers:
+            flag = True 
+            break
+    if flag:
+        My_LineBotAPI.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text='Nanomachine, son! Input Error!',
+            )
+        )  
+    else: 
+        try:
+            res = str(int(eval(formula)))
+        except ZeroDivisionError:
+            res = "Divided by Zero, unacceptable"
+        My_LineBotAPI.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text=res,
+            )
+        ) 
+
+
+# Line Sticker Class
+class My_Sticker:
+    def __init__(self, p_id: str, s_id: str):
+        self.type = 'sticker'
+        self.packageID = p_id
+        self.stickerID = s_id
+
+'''
+See more about Line Sticker, references below
+> Line Developer Message API, https://developers.line.biz/en/reference/messaging-api/#sticker-message
+> Line Bot Free Stickers, https://developers.line.biz/en/docs/messaging-api/sticker-list/
+'''
+# Add stickers into my_sticker list
+my_sticker = [My_Sticker(p_id='446', s_id='1995'), My_Sticker(p_id='446', s_id='2012'),
+     My_Sticker(p_id='446', s_id='2024'), My_Sticker(p_id='446', s_id='2027'),
+     My_Sticker(p_id='789', s_id='10857'), My_Sticker(p_id='789', s_id='10877'),
+     My_Sticker(p_id='789', s_id='10881'), My_Sticker(p_id='789', s_id='10885'),
+     ]
+
+# Line Sticker Event
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker(event):
+    # Random choice a sticker from my_sticker list
+    ran_sticker = random.choice(my_sticker)
+    # Reply Sticker Message
+    My_LineBotAPI.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id= ran_sticker.packageID,
+            sticker_id= ran_sticker.stickerID
+        )
+    )
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app='main:app', reload=True, host='0.0.0.0', port=8787)
